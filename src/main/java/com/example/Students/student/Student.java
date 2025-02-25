29fe8cff57552ce463c181a25475935367a8a391
Tony Kanyamuka <tonykanyamuka@gmail.com>
2025-02-25 23:30:37 +0200

    Configurations : configured dot-env,livereload,database jpa,connected with backend,modified studentmodel to make it a table

--- src/main/java/com/example/Students/student/Student.java
+++ src/main/java/com/example/Students/student/Student.java
@@ -1,15 +1,28 @@
 package com.example.Students.student;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
 
+@Entity
+@Table
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
